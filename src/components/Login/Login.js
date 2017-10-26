4e9bebf963495787bedacdb76ef8d9e5f8890d47
chrisjxn <usjacksons@gmail.com>
2017-10-26 17:03:44 -0600

    working site

---
+++ src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Login.css';
+import logo from './communityBank.svg';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className='Login'>
+                <img src={logo} alt="Community Bank logo" />
+                <a href={process.env.REACT_APP_LOGIN}>
+                    <button>Login</button>
+                </a>
+            </div>
+        )
+    }
+}
